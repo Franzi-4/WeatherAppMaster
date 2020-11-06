197ae196997bdad9e2eabd9122e70a42c9056f32
Franzi-4 <67898573+Franzi-4@users.noreply.github.com>
2020-11-06 09:08:38 +0100

    Change City

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-      <Temperature defaultCity="Chicago"/>
+      <Temperature defaultCity="Berlin"/>
       <footer>
         <a href="https://github.com/Franzi-4" target="_blank" rel="noopener noreferrer"> Open-source code</a> by Franzi
       </footer>
